d0b0af0ca606d6f4ecffb00579a3ccc0d646150a
Mary Jack <maryjackinthebox@gmail.com>
2018-04-22 19:04:50 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -37,11 +37,13 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.map do |season, holidays|
-    holidays.map do |holiday, supplies|
-      holiday if supplies.include?("BBQ")
-    end
-  end
+  bbqarray = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |name, supplies|
+      bbqarray << name if supplies.include?('BBQ')
+    end #if
+  end#do
+  bbqarray
 end
 
 
