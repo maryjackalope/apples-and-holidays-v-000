1c1a33dbd38dcb90573100e6bb3c3b3d4d2a2c75
Mary Jack <maryjackinthebox@gmail.com>
2018-04-22 18:59:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,6 +19,7 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season][holiday_name]=supply_array
+  holiday_hash #remember to return
 end
 
 def all_winter_holiday_supplies(holiday_hash)
